519ed4094ca0d03312325e4b2a07b441b7b455f3
mregrock <78217843+mregrock@users.noreply.github.com>
2021-04-18 23:11:52 +0300

    Скачал api

--- main.py
+++ main.py
@@ -1,1 +1,19 @@
-print("Hello world!")
+from telegram.ext import Updater, MessageHandler, Filters
+from telegram.ext import CallbackContext, CommandHandler
+
+
+def echo(update, context):
+    update.message.reply_text(update.message.text)
+
+
+def main():
+    updater = Updater("1686818986:AAGY4skTGpvDXKA7iWLgWy3RzpHz5YPAx1A", use_context=True)
+    dp = updater.dispatcher
+    text_handler = MessageHandler(Filters.text, echo)
+    dp.add_handler(text_handler)
+    updater.start_polling()
+    updater.idle()
+
+
+if __name__ == '__main__':
+    main()
