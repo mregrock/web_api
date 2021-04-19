eb1680200d81a8a465781355528c073db61d5493
AvGust-Suns <78925270+AvGust-Suns@users.noreply.github.com>
2021-04-19 19:57:28 +0300

    Версия 0.0.0.2: *Добавлена клава и пофикшен баг со слэшем

--- main.py
+++ main.py
@@ -1,16 +1,24 @@
 from telegram.ext import Updater, MessageHandler, Filters
 from telegram.ext import CallbackContext, CommandHandler
+from telegram import ReplyKeyboardMarkup
+
+
+reply_keyboard = [['/start'],
+                  ['/help']]
+markup = ReplyKeyboardMarkup(reply_keyboard, one_time_keyboard=False)
 
 
 def start(update, context):
     update.message.reply_text(
         "Привет! Я Магнитола Бот. Напишите мне номер команды и я их выполню.\n"
-        + "Напиши мне \\help для большей информации.")
+        + "Напиши мне /help для большей информаци.",
+        reply_markup=markup)
 
 
 def help(update, context):
     update.message.reply_text(
-        "Номера команд")
+        "Номера команд",
+        reply_markup=markup)
 
 
 def music(update, context):
