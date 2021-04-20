fffd43712c515bd327b317fc073fd741286925f7
mregrock <78217843+mregrock@users.noreply.github.com>
2021-04-20 23:21:47 +0300

    Поиск треков с возможностью выбора

--- main.py
+++ main.py
@@ -9,6 +9,7 @@ reply_keyboard = [['/start', '/help'],
 markup = ReplyKeyboardMarkup(reply_keyboard, one_time_keyboard=False)
 updater = Updater("1686818986:AAGY4skTGpvDXKA7iWLgWy3RzpHz5YPAx1A", use_context=True)
 client = yandex_music.Client.from_credentials('mregrock@yandex.ru', '561867603egorkAgren$5')
+top = list()
 
 
 def start(update, context):
@@ -39,31 +40,37 @@ def search_track(update, context):
 
 
 def music(update, context):
+    global top
+    download_file = ""
+    top = list()
+    dp = updater.dispatcher
+    dp.remove_handler(MessageHandler(Filters.text, music))
     name_track = update.message.text
     search_result = client.search(name_track)
     text = [f'Результаты по запросу "{name_track}":', '']
     best_result_text = ''
-    download_file = ""
     if search_result.best:
-        type_ = search_result.best.type
-        best = search_result.best.result
-        if type_ in ['track', 'podcast_episode']:
-            artists = ''
-            if best.artists:
-                artists = ' - ' + ', '.join(artist.name for artist in best.artists)
-            best_result_text = best.title + artists
-            download_file = best_result_text + ".mp3"
-            best.download(download_file)
-        elif type_ == 'artist':
-            best_result_text = best.name
-        elif type_ in ['album', 'podcast']:
-            best_result_text = best.title
-        elif type_ == 'playlist':
-            best_result_text = best.title
-        elif type_ == 'video':
-            best_result_text = f'{best.title} {best.text}'
+        for i in range(min(5, len(search_result.tracks.results))):
+            type_ = search_result.tracks.results[i].type
+            best = search_result.tracks.results[i]
+            if type_ in ['track', 'podcast_episode', 'music']:
+                artists = ''
+                if best.artists:
+                    artists = ' - ' + ', '.join(artist.name for artist in best.artists)
+                best_result_text = best.title + artists
+                download_file = best_result_text + ".mp3"
+                best.download(download_file)
+                top.append(download_file)
+            elif type_ == 'artist':
+                best_result_text = best.name
+            elif type_ in ['album', 'podcast']:
+                best_result_text = best.title
+            elif type_ == 'playlist':
+                best_result_text = best.title
+            elif type_ == 'video':
+                best_result_text = f'{best.title} {best.text}'
 
-        text.append(f'Содержимое лучшего результата: {best_result_text}\n')
+            text.append(f'{i + 1}. {best_result_text}\n')
 
     if search_result.artists:
         text.append(f'Исполнителей: {search_result.artists.total}')
@@ -77,8 +84,11 @@ def music(update, context):
         text.append(f'Видео: {search_result.videos.total}')
     text.append('')
     update.message.reply_text('\n'.join(text), reply_markup=markup)
-    update.message.reply_audio(open(download_file, 'rb'), reply_markup=markup)
-    os.remove(download_file)
+
+
+def choose_track(update, context):
+    update.message.reply_audio(open(top[int(update.message.text[8:]) - 1], 'rb'), reply_markup=markup)
+    os.remove(top[int(update.message.text[8:]) - 1])
 
 
 def main():
@@ -87,6 +97,7 @@ def main():
     dp.add_handler(CommandHandler("help", help))
     dp.add_handler(CommandHandler("close_keyboard", close_keyboard))
     dp.add_handler(CommandHandler("search_track", search_track))
+    dp.add_handler(CommandHandler("choose", choose_track))
     updater.start_polling()
     updater.idle()
 
