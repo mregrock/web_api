db12d1db51c60f66157f87197aab5debfa330410
mregrock <78217843+mregrock@users.noreply.github.com>
2021-04-20 22:18:17 +0300

    Первая версия поиска трека

--- main.py
+++ main.py
@@ -1,13 +1,15 @@
 from telegram.ext import Updater, MessageHandler, Filters
 from telegram.ext import CallbackContext, CommandHandler
-from telegram import ReplyKeyboardMarkup, ReplyKeyboardRemove
+from telegram import ReplyKeyboardMarkup, ReplyKeyboardRemove, InputMediaAudio
 import yandex_music
+import os
 
 reply_keyboard = [['/start', '/help'],
                   ['/close_keyboard', '/search_track']]
 markup = ReplyKeyboardMarkup(reply_keyboard, one_time_keyboard=False)
 updater = Updater("1686818986:AAGY4skTGpvDXKA7iWLgWy3RzpHz5YPAx1A", use_context=True)
-client = yandex_music.Client()
+client = yandex_music.Client.from_credentials('mregrock@yandex.ru', '561867603egorkAgren$5')
+
 
 def start(update, context):
     update.message.reply_text(
@@ -39,23 +41,44 @@ def search_track(update, context):
 def music(update, context):
     name_track = update.message.text
     search_result = client.search(name_track)
-    type_ = search_result.best.type
-    best = search_result.best.result
-    best_result_text = ""
-    if type_ in ['track', 'podcast_episode']:
-        artists = ''
-        if best.artists:
-            artists = ' - ' + ', '.join(artist.name for artist in best.artists)
-        best_result_text = best.title + artists
-    elif type_ == 'artist':
-        best_result_text = best.name
-    elif type_ in ['album', 'podcast']:
-        best_result_text = best.title
-    elif type_ == 'playlist':
-        best_result_text = best.title
-    elif type_ == 'video':
-        best_result_text = f'{best.title} {best.text}'
-    print(best_result_text)
+    text = [f'Результаты по запросу "{name_track}":', '']
+    best_result_text = ''
+    download_file = ""
+    if search_result.best:
+        type_ = search_result.best.type
+        best = search_result.best.result
+        if type_ in ['track', 'podcast_episode']:
+            artists = ''
+            if best.artists:
+                artists = ' - ' + ', '.join(artist.name for artist in best.artists)
+            best_result_text = best.title + artists
+            download_file = best_result_text + ".mp3"
+            best.download(download_file)
+        elif type_ == 'artist':
+            best_result_text = best.name
+        elif type_ in ['album', 'podcast']:
+            best_result_text = best.title
+        elif type_ == 'playlist':
+            best_result_text = best.title
+        elif type_ == 'video':
+            best_result_text = f'{best.title} {best.text}'
+
+        text.append(f'Содержимое лучшего результата: {best_result_text}\n')
+
+    if search_result.artists:
+        text.append(f'Исполнителей: {search_result.artists.total}')
+    if search_result.albums:
+        text.append(f'Альбомов: {search_result.albums.total}')
+    if search_result.tracks:
+        text.append(f'Треков: {search_result.tracks.total}')
+    if search_result.playlists:
+        text.append(f'Плейлистов: {search_result.playlists.total}')
+    if search_result.videos:
+        text.append(f'Видео: {search_result.videos.total}')
+    text.append('')
+    update.message.reply_text('\n'.join(text), reply_markup=markup)
+    update.message.reply_audio(open(download_file, 'rb'), reply_markup=markup)
+    os.remove(download_file)
 
 
 def main():
