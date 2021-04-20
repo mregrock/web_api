2fae6ac9fa63dffe696a95c1aea6c70c2a605815
mregrock <78217843+mregrock@users.noreply.github.com>
2021-04-20 20:22:10 +0300

    Откатил предыдущую версию

--- main.py
+++ main.py
@@ -1,42 +1,50 @@
-import telebot
 from telegram.ext import Updater, MessageHandler, Filters
 from telegram.ext import CallbackContext, CommandHandler
 from telegram import ReplyKeyboardMarkup, ReplyKeyboardRemove
 
-bot = telebot.TeleBot('1686818986:AAGY4skTGpvDXKA7iWLgWy3RzpHz5YPAx1A')
-
-keyboard1 = telebot.types.ReplyKeyboardMarkup(True, True)
-keyboard2 = telebot.types.ReplyKeyboardMarkup(True, True)
-keyboard1.row('/start', '/Search_music')
-keyboard1.add('/help', '/close_keyboard')
+reply_keyboard = [['/start', '/help'],
+                  ['/close_keyboard', '/search_track']]
+markup = ReplyKeyboardMarkup(reply_keyboard, one_time_keyboard=False)
 
 
-@bot.message_handler(commands=['start'])
-def start_message(message):
-    bot.send_message(message.chat.id, "Привет! Я Магнитола Бот. Напишите мне номер команды и я их выполню.\n"
-                     + "Напиши мне /help для большей информации.",
-                     reply_markup=keyboard1)
+def start(update, context):
+    update.message.reply_text(
+        "Привет! Я Магнитола Бот. Напишите мне номер команды и я их выполню.\n"
+        + "Напиши мне /help для большей информации.",
+        reply_markup=markup)
 
 
-@bot.message_handler(commands=['help'])
-def help_message(message):
-    bot.send_message(message.chat.id, 'Номера команд',
-                     reply_markup=keyboard1)
+def help(update, context):
+    update.message.reply_text(
+        "Номера команд",
+        reply_markup=markup)
 
 
-@bot.message_handler(commands=['close_keyboard'])
-def close_keyboard_message(message):
-    bot.send_message(message.chat.id, 'Принял к сведению',
-                     reply_markup=keyboard2)
+def close_keyboard(update, context):
+    update.message.reply_text(
+        "Принял к сведению.",
+        reply_markup=ReplyKeyboardRemove()
+    )
 
 
-@bot.message_handler(content_types=['text'])
-def send_songs(message):
-    if message.text == '/Search_music':
-        audio = open('123.mp3', 'rb')
-        bot.send_chat_action(message.from_user.id, 'upload_audio')
-        bot.send_audio(message.from_user.id, audio)
-        audio.close()
+def search_track(update, context):
+    update.message.reply_text("Введите название песни", reply_markup=markup)
 
 
-bot.polling()
+def music(update, context):
+    update.message.reply_text(update.message.text)
+
+
+def main():
+    updater = Updater("1686818986:AAGY4skTGpvDXKA7iWLgWy3RzpHz5YPAx1A", use_context=True)
+    dp = updater.dispatcher
+    dp.add_handler(CommandHandler("start", start))
+    dp.add_handler(CommandHandler("help", help))
+    dp.add_handler(CommandHandler("close_keyboard", close_keyboard))
+    dp.add_handler(CommandHandler("search_music", search_track))
+    updater.start_polling()
+    updater.idle()
+
+
+if __name__ == '__main__':
+    main()
