9ffee099aac07dc79c5d10fd66e97c75a0828e51
mregrock <78217843+mregrock@users.noreply.github.com>
2021-04-22 20:34:59 +0300

    Добавлена возможность оценивания альбомов

--- main.py
+++ main.py
@@ -33,13 +33,15 @@ def close_keyboard(update, context):
     )
 
 
-class GiveScore:
+class AddAlbum:
     def __init__(self):
         self.top = list()
         self.text_handler = None
         self.dp = updater.dispatcher
+        self.user_id = 0
+        self.album_id = 0
 
-    def find_album(self, update, context):
+    def add_album(self, update, context):
         self.top = list()
         self.text_handler = None
         connect = sqlite3.connect("magnitola_db.db")
@@ -91,13 +93,13 @@ class GiveScore:
             albums = cursor.execute("SELECT album_yandex_id FROM albums").fetchall()
             connect.commit()
             album = self.top[number][1]
-            self.albom_id = album.id
+            self.album_id = album.id
             if (album.id,) not in albums:
                 artists = ', '.join(artist.name for artist in album.artists)
                 cursor.execute("INSERT INTO albums(album_name, album_artist, album_yandex_id) VALUES(?, ?, ?)",
                                (album.title, artists, album.id))
                 connect.commit()
-            update.message.reply_text("Введите оценку альбома(от 0 до 10)")
+            update.message.reply_text("Введите оценку альбому")
             self.text_handler = MessageHandler(Filters.text, self.give_num_score)
             self.dp.add_handler(self.text_handler)
         except ValueError:
@@ -113,80 +115,15 @@ class GiveScore:
         connect = sqlite3.connect("magnitola_db.db")
         cursor = connect.cursor()
         connect.commit()
-        print(self.albom_id, self.user_id, number)
+        albums_scores = cursor.execute("SELECT album_id, telegram_user_id FROM users_score").fetchall()
+        if (self.album_id, self.user_id, ) in albums_scores:
+            cursor.execute("DELETE FROM users_score WHERE album_id == ? AND telegram_user_id == ?",
+                           (self.album_id, self.user_id, ))
+            connect.commit()
         cursor.execute("INSERT INTO users_score(album_id, telegram_user_id, score) VALUES(?, ?, ?)",
-                       (self.albom_id, self.user_id, number))
+                       (self.album_id, self.user_id, number))
         connect.commit()
         update.message.reply_text("Ваша оценка очень важна для нас")
-
-
-class AddAlbum:
-    def __init__(self):
-        self.top = list()
-        self.text_handler = None
-        self.dp = updater.dispatcher
-
-    def add_album(self, update, context):
-        self.top = list()
-        self.text_handler = None
-        connect = sqlite3.connect("magnitola_db.db")
-        cursor = connect.cursor()
-        user_id = int(update.message.chat_id)
-        telegram_ids = cursor.execute("SELECT telegram_id FROM users").fetchall()
-        connect.commit()
-        if (user_id,) not in telegram_ids:
-            cursor.execute("INSERT INTO users(telegram_id) VALUES(?)", (user_id,))
-            connect.commit()
-        self.text_handler = MessageHandler(Filters.text, self.search_album)
-        self.dp.add_handler(self.text_handler)
-        update.message.reply_text("Введите название альбома", reply_markup=markup)
-
-    def search_album(self, update, context):
-        self.dp.remove_handler(self.text_handler)
-        name_album = update.message.text
-        search_result = client.search(name_album)
-        text = [f'Результаты по запросу "{name_album}":', '']
-        download_file = ""
-        best_result_text = ''
-        if search_result.best:
-            for i in range(min(5, len(search_result.albums.results))):
-                type_ = search_result.albums.results[i].type
-                best = search_result.albums.results[i]
-                artists = ''
-                if best.artists:
-                    artists = ' - ' + ', '.join(artist.name for artist in best.artists)
-                best_result_text = best.title + artists
-                download_file = best_result_text + ".jpg"
-                self.top.append((download_file, search_result.albums.results[i]))
-                text.append(f'{i + 1}. {best_result_text}\n')
-        update.message.reply_text('\n'.join(text), reply_markup=markup)
-        update.message.reply_text("Введите номер альбома")
-        self.text_handler = MessageHandler(Filters.text, self.choose_album)
-        self.dp.add_handler(self.text_handler)
-
-    def choose_album(self, update, context):
-        try:
-            number = int(update.message.text) - 1
-            self.top[number][1].download_cover(self.top[number][0])
-            text = [self.top[number][0][:-4], '', f"Количество треков: {self.top[number][1].track_count}"]
-            update.message.reply_text('\n'.join(text), reply_markup=markup)
-            update.message.reply_photo(open(self.top[number][0], 'rb'), reply_markup=markup)
-            os.remove(self.top[int(update.message.text) - 1][0])
-            self.dp.remove_handler(self.text_handler)
-            connect = sqlite3.connect("magnitola_db.db")
-            cursor = connect.cursor()
-            albums = cursor.execute("SELECT album_yandex_id FROM albums").fetchall()
-            connect.commit()
-            album = self.top[number][1]
-            if (album.id,) not in albums:
-                artists = ', '.join(artist.name for artist in album.artists)
-                cursor.execute("INSERT INTO albums(album_name, album_artist, album_yandex_id) VALUES(?, ?, ?)",
-                               (album.title, artists, album.id))
-                connect.commit()
-        except ValueError:
-            update.message.reply_text("Ошибка! Введите число")
-        except IndexError:
-            update.message.reply_text("Ошибка! Введите корректное число")
 
 
 class MusicSearch:
@@ -255,13 +192,11 @@ def main():
     dp = updater.dispatcher
     music_searcher = MusicSearch()
     adder_album = AddAlbum()
-    giver_score = GiveScore()
     dp.add_handler(CommandHandler("start", start))
     dp.add_handler(CommandHandler("help", help))
     dp.add_handler(CommandHandler("close_keyboard", close_keyboard))
     dp.add_handler(CommandHandler("search_track", music_searcher.search_track))
     dp.add_handler(CommandHandler("add_album", adder_album.add_album))
-    dp.add_handler(CommandHandler("give_score", giver_score.find_album))
     updater.start_polling()
     updater.idle()
 
