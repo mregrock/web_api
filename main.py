b170adc2ff1e5374d7e2c4cb7bc1a8c31f709157
mregrock <78217843+mregrock@users.noreply.github.com>
2021-04-22 21:57:54 +0300

    Исправлена ошибка связанная с различными командами

--- main.py
+++ main.py
@@ -11,6 +11,7 @@ reply_keyboard = [['/start', '/help'],
 markup = ReplyKeyboardMarkup(reply_keyboard, one_time_keyboard=False)
 updater = Updater("1686818986:AAGY4skTGpvDXKA7iWLgWy3RzpHz5YPAx1A", use_context=True)
 client = yandex_music.Client.from_credentials('mregrock@yandex.ru', '561867603egorkAgren$5')
+text_handlers = list()
 
 
 def start(update, context):
@@ -33,6 +34,13 @@ def close_keyboard(update, context):
     )
 
 
+def clear_handlers():
+    global text_handlers
+    dp = updater.dispatcher
+    for i in text_handlers:
+        dp.remove_handler(i)
+
+
 class AddAlbum:
     def __init__(self):
         self.top = list()
@@ -42,8 +50,10 @@ class AddAlbum:
         self.album_id = 0
 
     def add_album(self, update, context):
+        global text_handlers
         self.top = list()
         self.text_handler = None
+        clear_handlers()
         connect = sqlite3.connect("magnitola_db.db")
         cursor = connect.cursor()
         self.user_id = int(update.message.chat_id)
@@ -54,10 +64,13 @@ class AddAlbum:
             connect.commit()
         self.text_handler = MessageHandler(Filters.text, self.search_album)
         self.dp.add_handler(self.text_handler)
+        text_handlers.append(self.text_handler)
         update.message.reply_text("Введите название альбома", reply_markup=markup)
 
     def search_album(self, update, context):
+        global text_handlers
         self.dp.remove_handler(self.text_handler)
+        del text_handlers[text_handlers.index(self.text_handler)]
         name_album = update.message.text
         search_result = client.search(name_album)
         text = [f'Результаты по запросу "{name_album}":', '']
@@ -77,9 +90,11 @@ class AddAlbum:
         update.message.reply_text('\n'.join(text), reply_markup=markup)
         update.message.reply_text("Введите номер альбома")
         self.text_handler = MessageHandler(Filters.text, self.choose_album)
+        text_handlers.append(self.text_handler)
         self.dp.add_handler(self.text_handler)
 
     def choose_album(self, update, context):
+        global text_handlers
         try:
             number = int(update.message.text) - 1
             self.top[number][1].download_cover(self.top[number][0])
@@ -87,6 +102,7 @@ class AddAlbum:
             update.message.reply_text('\n'.join(text), reply_markup=markup)
             update.message.reply_photo(open(self.top[number][0], 'rb'), reply_markup=markup)
             os.remove(self.top[int(update.message.text) - 1][0])
+            del text_handlers[text_handlers.index(self.text_handler)]
             self.dp.remove_handler(self.text_handler)
             connect = sqlite3.connect("magnitola_db.db")
             cursor = connect.cursor()
@@ -101,6 +117,7 @@ class AddAlbum:
                 connect.commit()
             update.message.reply_text("Введите оценку альбому")
             self.text_handler = MessageHandler(Filters.text, self.give_num_score)
+            text_handlers.append(self.text_handler)
             self.dp.add_handler(self.text_handler)
         except ValueError:
             update.message.reply_text("Ошибка! Введите число")
@@ -108,10 +125,12 @@ class AddAlbum:
             update.message.reply_text("Ошибка! Введите корректное число")
 
     def give_num_score(self, update, context):
+        global text_handlers
         number = int(update.message.text)
         if 0 > number > 10:
             number = 5
         self.dp.remove_handler(self.text_handler)
+        del text_handlers[text_handlers.index(self.text_handler)]
         connect = sqlite3.connect("magnitola_db.db")
         cursor = connect.cursor()
         connect.commit()
@@ -133,15 +152,20 @@ class MusicSearch:
         self.dp = updater.dispatcher
 
     def search_track(self, update, context):
+        global text_handlers
         self.top = list()
         self.text_handler = None
+        clear_handlers()
         update.message.reply_text("Введите название песни", reply_markup=markup)
         self.text_handler = MessageHandler(Filters.text, self.music)
+        text_handlers.append(self.text_handler)
         self.dp.add_handler(self.text_handler)
 
     def music(self, update, context):
+        global text_handlers
         download_file = ""
         self.dp.remove_handler(self.text_handler)
+        del text_handlers[text_handlers.index(self.text_handler)]
         name_track = update.message.text
         search_result = client.search(name_track)
         text = [f'Результаты по запросу "{name_track}":', '']
@@ -173,14 +197,17 @@ class MusicSearch:
         update.message.reply_text('\n'.join(text), reply_markup=markup)
         update.message.reply_text("Введите номер трека")
         self.text_handler = MessageHandler(Filters.text, self.choose_track)
+        text_handlers.append(self.text_handler)
         self.dp.add_handler(self.text_handler)
 
     def choose_track(self, update, context):
+        global text_handlers
         try:
             number = int(update.message.text) - 1
             self.top[number][1].download(self.top[number][0])
             update.message.reply_audio(open(self.top[number][0], 'rb'), reply_markup=markup)
             os.remove(self.top[int(update.message.text) - 1][0])
+            del text_handlers[text_handlers.index(self.text_handler)]
             self.dp.remove_handler(self.text_handler)
         except ValueError:
             update.message.reply_text("Ошибка! Введите число")
